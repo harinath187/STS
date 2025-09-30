3c18737a802d0ffda66b2f823f9a9500e467f100
ranjeet-kumar-2002 <rk1109533@gmail.com>
2025-09-30 11:40:42 +0530

    done

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,14 @@
 from flask import Flask
 from app.routes.login_routes import register_routes
-<<<<<<< HEAD
 from app.routes.emp_dashboard_routes import register_employee_routes
 from app.routes.test import test_routes  # Blueprint
 from app.routes.support_ticket_routes import register_support_ticket_routes
 from app.routes.task_comment_routes import register_task_comments_routes
 from app.routes.support_comment_routes import register_support_comment_routes  # <-- new
-
-=======
 from app.routes.it_manager_routes import register_support_routes
 from app.routes.test import test_routes  # ✅ Add this
 from app.routes.emp_dashboard_routes import register_employee_routes
 from app.routes.supporthistoryroute import supporthistory,suppassignlist,notsuppassignlist
->>>>>>> 4e57d765e0a57f68336b399ae94db30129662cac
 def create_app():
     app = Flask(__name__)
     app.secret_key = "sts"
--- run.py
+++ run.py
@@ -1,28 +1,5 @@
-import os
-from flask import Flask
-from app.routes.login_routes import register_routes
-from app.routes.manager_routes import manager_bp
-
-
-base_dir = os.path.dirname(__file__)
-template_dir = os.path.join(base_dir, "app", "templates")
-static_dir = os.path.join(base_dir, "app", "static")
-
-app = Flask(
-    __name__, 
-    template_folder=template_dir, 
-    static_folder=static_dir
-)
-
-#application secret key
-app.secret_key = "your-secret-key"
-
-# Register routes
-register_routes(app)
-app.register_blueprint(manager_bp)
+from app import create_app
+app = create_app()
 
 if __name__ == "__main__":
-    print("Template folder is:", app.template_folder)
-    print("Static folder is:", app.static_folder)
-    app.run(debug=True) 
-    
+    app.run(debug=True)
