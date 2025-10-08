897e310197203f92f0f1cfce883f1f48dfc3adbe
saideekshitha2621 <sai.deekshitha@ampera.tech>
2025-10-08 19:10:01 +0530

    updatechanges

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ from app import create_app
 from flask import session
 app = create_app()
 
-
 @app.context_processor
 def inject_user():
     return dict(user=session.get("user"))
+
 
 if __name__ == "__main__":
     print("Template folder is:", app.template_folder)
